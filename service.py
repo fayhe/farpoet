16e0ed27eab985510047935a87a94d5c98f22c66
fay <fay@fays-MacBook.local>
2017-01-10 22:29:40 +0800

    change

--- service.py
+++ service.py
@@ -3,17 +3,29 @@ import jieba
 import jieba.analyse
 
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
-@app.route("/")
-def hello():
-    with open('beihaidao_index') as data_file:    
+@app.route('/gettags/<indexfile>',  methods=['POST'])
+def gettags(indexfile):
+    print "enter!!!!"	
+    with open('/Users/fay/Downloads/hh/' + indexfile) as data_file:    
     	youjis = json.load(data_file)
     text = ""
     for youji in youjis:
 		text = text + youji["tags"]   
-    tags = jieba.analyse.extract_tags(text, topK=100, allowPOS={'n'})	     		 
-    return ",".join(tags)
+    tags = jieba.analyse.extract_tags(text, topK=100, allowPOS={'n'})
+    tags_html = ""
+    for tag in tags:
+    	tags_html = tags_html + "<a href='#' onclick=\"change_search('" + tag + "');\">" + tag + "</a> "
+
+    print tags_html	     		 
+    return tags_html
+
+@app.route('/index')
+def root():
+    print "enter!!!!"
+    return app.send_static_file('index.html')
+   
 
 if __name__ == "__main__":
     app.run()
